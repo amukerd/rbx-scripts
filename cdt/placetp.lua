12318ccd5dd0345c067b857b7fa7b00d069ea478
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-06 14:21:12 -0400

    Update placetp.lua

--- cdt/placetp.lua
+++ cdt/placetp.lua
@@ -1,12 +1,11 @@
 local TeleportService = game:GetService("TeleportService")
-local player = game:GetService("Players").Players.LocalPlayer
+local u = game:GetService("Players").Players.LocalPlayer
 
 local ID1 = 1554960397
 local ID2 = 135202704953082
 
 if game.PlaceId == ID2 then
-    -- Already in the stands place, do nothing.
-    return
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/amukerd/rbx-scripts/refs/heads/main/cdt/autobuystands.lua"))()
 end
 
 while game.PlaceId == ID1 do
@@ -15,3 +14,23 @@ while game.PlaceId == ID1 do
     end)
     task.wait(5)
 end
+
+local BlockedUsers = {
+    [4512510904] = true,
+    [8083594000] = true,
+    [8083636321] = true,
+    [8083664487] = true,
+}
+
+task.spawn(function()
+    while true do
+        for _, player in ipairs(Players:GetPlayers()) do
+            if player ~= u and BlockedUsers[player.UserId] then
+                TeleportService:Teleport(ID1, u)
+                break
+            end
+        end
+
+        task.wait(5)
+    end
+end)
