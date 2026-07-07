c19c45c2b9db37a59f31404fa31a6b8a3b1a407b
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-07 19:37:36 -0400

    Create customizationitems.lua

---
+++ cdt/customizationitems.lua
@@ -0,0 +1,10 @@
+{
+    PriceInTokens = 70000,
+    OfferId = "2fbb8998-f1cd-4b58-8cf3-3b590310004b",
+    Item = {
+        Type = "Customization",
+        Name = "Wings",
+        Category = "UnderglowTexture"
+    },
+    HiddenFromOthers = false
+},
