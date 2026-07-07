7a6f29b7731780a6fae12e627065db7414d2af22
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-07 16:21:02 -0400

    Change RAP_PERCENT from 0.90 to 0.85

--- cdt/autobuystands.lua
+++ cdt/autobuystands.lua
@@ -12,7 +12,7 @@ local OfferPurchaseRemote = ReplicatedStorage.Remotes.Services.TradingHubService
 
 local WebhookURL = "https://discord.com/api/webhooks/1480676513668923627/c-7JOdimxEYnh3Ol2DNcCuzHyPaCrZ015TTlDnGL3aM7Rg42zRJZhFSAc3qmqNK8t51I"
 
-local RAP_PERCENT = 0.90
+local RAP_PERCENT = 0.85
 local SCAN_INTERVAL = 1
 
 local activeThreads = {}
