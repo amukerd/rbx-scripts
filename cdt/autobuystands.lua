f41a3033e18d50d85ce4fc9d28c16aaa5cb2653d
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-07 15:42:06 -0400

    Update autobuystands.lua

--- cdt/autobuystands.lua
+++ cdt/autobuystands.lua
@@ -5,39 +5,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
 
-local BlockedUsers={[4512510904]=true,[8083594000]=true,[8083636321]=true,[8083664487]=true}
-
-task.spawn(function()
-    while task.wait(5) do
-        if #Players:GetPlayers() < 10 then
-            TeleportService:Teleport(1554960397, LocalPlayer)
-            return
-        end
-        for _, player in ipairs(Players:GetPlayers()) do
-            if player ~= LocalPlayer and BlockedUsers[player.UserId] then
-                TeleportService:Teleport(1554960397, LocalPlayer)
-                return
-            end
-        end
-    end
-end)
-
-setfpscap(20)
-
-if getconnections then
-    for _, connection in ipairs(getconnections(LocalPlayer.Idled)) do
-        if connection.Disable then
-            connection:Disable()
-        elseif connection.Disconnect then
-            connection:Disconnect()
-        end
-    end
-else
-    LocalPlayer.Idled:Connect(function()
-        VirtualUser:CaptureController()
-        VirtualUser:ClickButton2(Vector2.new())
-    end)
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/amukerd/rbx-scripts/refs/heads/main/cdt/extra.lua"))()
 
 local GetOffersRemote = ReplicatedStorage.Remotes.Services.TradingHubServiceRemotes.GetOffers
 local GetRapRemote = ReplicatedStorage.Remotes.Services.RapRemotes.GetRap
