c216d2544a64b1fe705cccd9ae9f5b19827a2206
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-09 17:13:23 -0400

    Update placetp.lua

--- cdt/placetp.lua
+++ cdt/placetp.lua
@@ -2,71 +2,70 @@ local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
 
-local localPlayer = Players.LocalPlayer
+local LocalPlayer = Players.LocalPlayer
 
 local LOBBY_ID = 1554960397
 local GAME_ID = 135202704953082
 
-if game.PlaceId == LOBBY_ID then
-    local ws = WebSocket.connect("ws://127.0.0.1:8080")
+local ws = WebSocket.connect("ws://127.0.0.1:8080")
 
-    local function send(data)
-        ws:Send(HttpService:JSONEncode(data))
+local function send(data)
+    ws:Send(HttpService:JSONEncode(data))
+end
+
+send(
+    {
+        type = "hello",
+        player = LocalPlayer.Name,
+        jobId = game.JobId,
+        players = #Players:GetPlayers()
+    }
+)
+
+ws.OnMessage:Connect(
+    function(msg)
+        local data = HttpService:JSONDecode(msg)
+
+        if data.type == "serverResponse" then
+            print("Teleporting to", data.jobId)
+
+            TeleportService:TeleportToPlaceInstance(GAME_ID, data.jobId, LocalPlayer)
+        end
     end
+)
 
-    send(
-        {
-            type = "hello",
-            player = localPlayer.Name,
-            jobId = game.JobId,
-            players = #Players:GetPlayers()
-        }
-    )
-
+if game.PlaceId == LOBBY_ID then
     task.wait(2)
 
     send(
         {
             type = "requestServer",
-            player = localPlayer.Name
+            player = LocalPlayer.Name,
+            jobId = game.JobId,
+            reason = "initial_join"
         }
     )
-
-    ws.OnMessage:Connect(function(msg)
-        local data = HttpService:JSONDecode(msg)
-
-        if data.type == "serverResponse" then
-            print(
-                "Teleporting to",
-                data.jobId
-            )
-
-            TeleportService:TeleportToPlaceInstance(
-                GAME_ID,
-                data.jobId,
-                localPlayer
-            )
-        end
-    end)
 end
 
 if game.PlaceId == GAME_ID then
-    task.spawn(function()
-        while task.wait(5) do
-            if #Players:GetPlayers() < 20 then
-                ws:Send(HttpService:JSONEncode({
-                    type = "requestServer",
-                    player = LocalPlayer.Name,
-                    jobId = game.JobId,
-                    reason = "low_population"
-                }))
-                break
+    task.spawn(
+        function()
+            while task.wait(5) do
+                if #Players:GetPlayers() < 20 then
+                    send(
+                        {
+                            type = "requestServer",
+                            player = LocalPlayer.Name,
+                            jobId = game.JobId,
+                            reason = "low_population"
+                        }
+                    )
+
+                    break
+                end
             end
         end
-    end)
+    )
 
-    loadstring(game:HttpGet(
-        "https://amukerd.github.io/rbx-scripts/cdt/autobuystands.lua"
-    ))()
-    return
+    loadstring(game:HttpGet("https://amukerd.github.io/rbx-scripts/cdt/autobuystands.lua"))()
 end
