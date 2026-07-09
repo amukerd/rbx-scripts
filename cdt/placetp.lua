85a563be7f24a1f627958b9c9846419275afe12a
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-09 01:30:09 -0400

    Update placetp.lua

--- cdt/placetp.lua
+++ cdt/placetp.lua
@@ -16,29 +16,34 @@ if game.PlaceId == 1554960397 then
     local localPlayer = Players.LocalPlayer
     local playerRank = playerRanks[localPlayer.Name]
     
-    local servers = {}
-    local req = game:HttpGet("https://games.roblox.com/v1/games/135202704953082/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true")
-    local body = HttpService:JSONDecode(req)
-    
-    if body and body.data then
-        for i, v in next, body.data do
-            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) then
-                if v.playing < 50 and v.playing < v.maxPlayers and v.id ~= JobId then
-                    table.insert(servers, v)
+    if playerRank then
+        local servers = {}
+        local req = game:HttpGet("https://games.roblox.com/v1/games/135202704953082/servers/Public?sortOrder=Desc&limit=100")
+        local body = HttpService:JSONDecode(req)
+        
+        if body and body.data then
+            for i, v in next, body.data do
+                if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) then
+                    if v.playing > 0 and v.playing < 50 and v.playing < v.maxPlayers and v.id ~= JobId then
+                        table.insert(servers, {
+                            id = v.id,
+                            playing = tonumber(v.playing)
+                        })
+                    end
                 end
             end
         end
-    end
-    
-    table.sort(servers, function(a, b)
-        return a.playing > b.playing
-    end)
-    
-    if #servers > 0 then
-        local targetIndex = math.min(playerRank, #servers)
-        local targetServerId = servers[targetIndex].id
         
-        TeleportService:TeleportToPlaceInstance(135202704953082, targetServerId, localPlayer)
+        table.sort(servers, function(a, b)
+            return a.playing > b.playing
+        end)
+        
+        if #servers > 0 then
+            local targetIndex = math.min(playerRank, #servers)
+            local targetServerId = servers[targetIndex].id
+            
+            TeleportService:TeleportToPlaceInstance(135202704953082, targetServerId, localPlayer)
+        end
     end
 end
 
