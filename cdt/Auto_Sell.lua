7bc27f425e4c98bf0641dfffcd47528b8a580db6
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-11 21:57:04 -0400

    Update Auto_Sell.lua

--- cdt/Auto_Sell.lua
+++ cdt/Auto_Sell.lua
@@ -1,33 +1,30 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 local HttpService = game:GetService("HttpService")
 
-local LocalPlayer = Players.LocalPlayer
-
 local WebhookURL = "https://discord.com/api/webhooks/1480676513668923627/c-7JOdimxEYnh3Ol2DNcCuzHyPaCrZ015TTlDnGL3aM7Rg42zRJZhFSAc3qmqNK8t51I"
+local requestFunc = http_request or request or (http and http.request) or HttpPost
 
-local CarService = ReplicatedStorage.Remotes.Services.CarServiceRemotes
+local TradingUtil = require(ReplicatedStorage.Util.TradingUtil)
 local TradingService = ReplicatedStorage.Remotes.Services.TradingHubServiceRemotes
-local RapService = ReplicatedStorage.Remotes.Services.RapRemotes
-local CustomizationItemsRemotes = require(ReplicatedStorage.Remotes.Services.CustomizationItemsRemotes)
-local CarCustomization = require(ReplicatedStorage.Databases.CarCustomization)
 
-local GetOwnedCars = CarService.GetOwnedCars
+local GetOwnedCars = ReplicatedStorage.Remotes.Services.CarServiceRemotes.GetOwnedCars
+local GetRap = ReplicatedStorage.Remotes.Services.RapRemotes.GetRap
 local OfferAdd = TradingService.OfferAdd
-local GetRap = RapService.GetRap
 local OnOfferAdded = TradingService.OnOfferAdded
 local OnOfferRemoved = TradingService.OnOfferRemoved
 
 local ListedOffers = {}
-
 local IconModule = {}
+
 pcall(function()
-    local requestFunc = http_request or request or (http and http.request) or HttpPost
     if requestFunc then
-		local res = requestFunc({
-			Url = "https://amukerd.github.io/rbx-scripts/cdt/Icons/Icon_Module.json",
-			Method = "GET"
-		})
+        local res = requestFunc({
+            Url = "https://amukerd.github.io/rbx-scripts/cdt/Icons/Icon_Module.json",
+            Method = "GET"
+        })
+
         if res and res.Body then
             IconModule = HttpService:JSONDecode(res.Body)
         end
@@ -39,70 +36,53 @@ local function formatNumber(num)
     return tostring(num):reverse():gsub("(%d%d%d)", "%1,"):reverse():gsub("^,", "")
 end
 
-local function sendWebhook(itemName, price, rapValue, sellerName)
-    local requestFunc = http_request or request or (http and http.request) or HttpPost
-    if not requestFunc then
-        return
-    end
-
-    task.spawn(
-        function()
-			local displayName = IconModule[itemName] or itemName
-			
-			local imageKey = tostring(itemName):gsub("/", "_")
-			
-			local imageUrl = "https://amukerd.github.io/rbx-scripts/cdt/Icons/" .. imageKey .. ".png"
-
-            local embedData = {
-                title = LocalPlayer.Name .. " Sold Offer",
-                color = 255,
-                fields = {
-                    {
-                        name = "Item Name",
-                        value = tostring(displayName),
-                        inline = true
-                    },
-                    {
-                        name = "Price",
-                        value = tostring(formatNumber(price)),
-                        inline = true
-                    },
-                    {
-                        name = "RAP",
-                        value = tostring(formatNumber(rapValue)),
-                        inline = true
-                    }
-                }
-            }
-
-            if imageUrl ~= "" then
-                embedData.thumbnail = {
-                    url = imageUrl
-                }
-            end
-
-            pcall(
-                function()
-                    requestFunc(
-                        {
-                            Url = WebhookURL,
-                            Method = "POST",
-                            Headers = {
-                                ["Content-Type"] = "application/json"
-                            },
-                            Body = HttpService:JSONEncode(
-                                {
-                                    embeds = {
-                                        embedData
-                                    }
-                                }
-                            )
-                        }
-                    )
-                end
-            )
-        end
-    )
+local function sendWebhook(itemName, price, rapValue)
+	task.spawn(function()
+	    local displayName = IconModule[itemName] or itemName
+		local imageKey = tostring(itemName):gsub("/", "_")
+		local imageUrl = "https://amukerd.github.io/rbx-scripts/cdt/Icons/" .. imageKey .. ".png"
+	
+	    local embedData = {
+	        title = LocalPlayer.Name .. " Sold Offer",
+	        color = 255,
+	        fields = {
+	            {
+	                name = "Item Name",
+	                value = tostring(displayName),
+	                inline = true
+	            },
+	            {
+	                name = "Price",
+	                value = tostring(formatNumber(price)),
+	                inline = true
+	            },
+	            {
+	                name = "RAP",
+	                value = tostring(formatNumber(rapValue)),
+	                inline = true
+	            }
+	        }
+	    }
+	
+	    embedData.thumbnail = {
+	        url = imageUrl
+	    }
+	
+	    requestFunc({
+	        Url = WebhookURL,
+	        Method = "POST",
+	        Headers = {
+	            ["Content-Type"] = "application/json"
+	        },
+	        Body = HttpService:JSONEncode(
+	            {
+	                embeds = {
+	                    embedData
+	                }
+	            }
+	        )
+	    })
+	end)
 end
 
 OnOfferAdded.OnClientEvent:Connect(function(player, offerData)
@@ -110,144 +90,52 @@ OnOfferAdded.OnClientEvent:Connect(function(player, offerData)
         return
     end
 
-    local offerId = offerData.OfferId
-    local item = offerData.Item
-
-    if item.Type == "Car" then
-
-        ListedOffers[offerId] = {
-            Name = item.Name,
-            RAP = offerData.PriceInTokens,
-            Price = offerData.PriceInTokens
-        }
-
-    elseif item.Type == "Customization" then
-        local key = item.Category .. "-" .. item.Name
-
-        ListedOffers[offerId] = {
-            Name = key,
-            RAP = offerData.PriceInTokens,
-            Price = offerData.PriceInTokens
-        }
-
+    if offerData.Item.Type ~= "Car" then
+        return
     end
 
-    print(
-        "Tracking offer:",
-        offerId,
-        ListedOffers[offerId].Name
-    )
+    ListedOffers[offerData.OfferId] = {
+        Name = offerData.Item.Name,
+        Price = offerData.PriceInTokens,
+        RAP = offerData.PriceInTokens
+    }
 end)
 
-local function listInventoryCars()
+local function listCars()
     local ownedCars = GetOwnedCars:InvokeServer()
 
-    if ownedCars then
-        for _, car in ipairs(ownedCars) do
-            task.spawn(
-                function()
-                    local carName = car.Name
-                    local carId = car.Id
+    for _, car in ipairs(ownedCars) do
+        if TradingUtil.CanTradeCar(LocalPlayer, car) then
+            local success, rap =
+                pcall(function()
+                    return GetRap:InvokeServer(car.Name)
+                end)
 
-                    local success, rap =
-                        pcall(
-                        function()
-                            return GetRap:InvokeServer(carName)
-                        end
-                    )
-
-                    if success and rap then
-                        rap = tonumber(rap) or 0
-                    
-                        if rap > 0 and rap < 250000 then
-                            OfferAdd:InvokeServer(
-                                {
-                                    Id = carId,
-                                    Type = "Car",
-                                    Name = carName
-                                },
-                                rap
-                            )
-                    
-                            print("Listed:", carName, rap)
-                        else
-                            print("Skipped:", carName, rap)
-                        end
-                    end
-                end
-            )
-        end
-    end
-end
-
---[[
-local function listCustomizationItems()
-
-    local items = CustomizationItemsRemotes.GetAll:InvokeServer()
-
-    for category, catItems in pairs(items) do
-        for name in pairs(catItems) do
-
-            if category == "Wrap" and name:match("OG") then
-                continue
-            end
-
-            local itemData = CarCustomization.GetItemData(category, name)
-            if itemData and itemData.Untradeable then
-                continue
-            end
-
-            local rapSuccess, rap = pcall(function()
-                return GetRap:InvokeServer(category .. "-" .. name)
-            end)
-
-            if rapSuccess and rap then
+            if success and rap then
                 rap = tonumber(rap) or 0
 
-                if rap > 0 and rap < 70000 then
-
-                    local key = category .. "-" .. name
-
-                    local success, err = OfferAdd:InvokeServer(
-                        {
-                            Type = "Customization",
-                            Category = category,
-                            Name = name
-                        },
-                        rap
-                    )
-
-                    if success then
-                        print("Listed customization:", key, rap)
-                    else
-                        warn("Failed:", key, err)
-                    end
+                if rap > 0 and rap < 250000 then
+                    local listed, err = OfferAdd:InvokeServer({
+                        Type = "Car",
+                        Name = car.Name,
+                        Id = car.Id
+                    },
+                    rap)
                 end
             end
-
-            task.wait(.05)
         end
     end
 end
-]]--
 
-listInventoryCars()
+listCars()
 
---listCustomizationItems()
-
-task.spawn(
-    function()
-        while true do
-            task.wait(300)
-
-            print("Refreshing booth listings...")
-
-            listInventoryCars()
-			
-			--listCustomizationItems()
-        end
+task.spawn(function()
+    while true do
+        task.wait(300)
+        print("Refreshing cars")
+        listCars()
     end
-)
+end)
 
 OnOfferRemoved.OnClientEvent:Connect(function(player, offerId)
     if player ~= LocalPlayer then
@@ -255,10 +143,6 @@ OnOfferRemoved.OnClientEvent:Connect(function(player, offerId)
     end
 
     local data = ListedOffers[offerId]
-    if not data then
-        return
-    end
-
     local currentRap = data.RAP
 
     local success, rap = pcall(function()
@@ -269,20 +153,8 @@ OnOfferRemoved.OnClientEvent:Connect(function(player, offerId)
         currentRap = tonumber(rap) or currentRap
     end
 
-    sendWebhook(
-        data.Name,
-        data.Price,
-        currentRap,
-        LocalPlayer.Name
-    )
-
+    sendWebhook(data.Name, data.Price, currentRap)
     ListedOffers[offerId] = nil
-
-    print(
-        "Sold:",
-        data.Name
-    )
 end)
 
 print("Auto_Sell Executed")
-
