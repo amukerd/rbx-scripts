2c71856e8a631dab60e506844e1659f152d80e1e
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-13 19:05:26 -0400

    Update example.lua

--- UI/example.lua
+++ UI/example.lua
@@ -3,7 +3,18 @@ local Library = loadstring(game:HttpGet("https://amukerd.github.io/rbx-scripts/U
 local Window = Library:CreateWindow("My Window")
 local Tab1 = Window:CreateTab("Main")
 
-Tab1:CreateButton("Do thing", function() print("clicked") end)
-Tab1:CreateToggle("Enable X", false, function(state) print(state) end)
-Tab1:CreateDropdown("Mode", {"A","B","C"}, "A", function(choice) print(choice) end)
-Tab1:CreateTextbox("Name", "Type here...", function(text) print(text) end)
+Tab1:CreateButton("Do thing", function()
+    print("clicked")
+end)
+
+Tab1:CreateToggle("Enable X", false, function(state)
+    print(state)
+end)
+
+Tab1:CreateDropdown("Mode", {"A","B","C"}, "A", function(choice)
+    print(choice) 
+end)
+
+Tab1:CreateTextbox("Name", "Type here...", function(text)
+    print(text)
+end)
