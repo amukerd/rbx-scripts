e9cd7027822a8b4c1e43d9d6ebf5b723eb144538
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-14 23:05:27 -0400

    Create antiafk.lua

---
+++ antiafk.lua
@@ -0,0 +1,5 @@
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    game:GetService("VirtualUser"):CaptureController()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+print("antiafk on")
