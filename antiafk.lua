0b67c5e1414b0846f97f4f9be1910b8bb0544001
Amukerd <146035837+amukerd@users.noreply.github.com>
2026-07-14 23:05:41 -0400

    Delete antiafk.lua

--- antiafk.lua
+++
@@ -1,5 +0,0 @@
-game:GetService("Players").LocalPlayer.Idled:Connect(function()
-    game:GetService("VirtualUser"):CaptureController()
-    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
-end)
-print("antiafk on")
